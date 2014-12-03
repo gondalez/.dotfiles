0da46d0e66735019eb5d22dda53a18fce1800639
Tristan Harmer <gondalez@gmail.com>
2014-12-03 17:32:51 +0800

    karabiner settings

--- mac/karabiner.sh
+++ mac/karabiner.sh
@@ -2,34 +2,36 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set option.tabmode_jl 1
+/bin/echo -n .
+$cli set private.control_space_is_underscore 1
+/bin/echo -n .
+$cli set repeat.initial_wait 200
+/bin/echo -n .
+$cli set option.tabmode_q 1
+/bin/echo -n .
 $cli set parameter.simultaneouskeypresses_pointingbutton_delay 0
 /bin/echo -n .
-$cli set repeat.initial_wait 250
-/bin/echo -n .
-$cli set remap.vim_emu_off_with_cmd_tab 1
-/bin/echo -n .
-$cli set parameter.wait_before_and_after_a_click_event 0
+$cli set remap.swapcolons 1
 /bin/echo -n .
 $cli set remap.programmer.shifts_parens 1
 /bin/echo -n .
-$cli set remap.vim_emu_off_at_spotlight 1
+$cli set option.tabmode_ik 1
 /bin/echo -n .
-$cli set remap.hjkl_arrow 1
-/bin/echo -n .
-$cli set option.tabmode_hl 1
-/bin/echo -n .
-$cli set remap.vim_emu_ 1
+$cli set remap.simultaneouskeypresses_ss_to_capslock 1
 /bin/echo -n .
 $cli set remap.tabmode 1
 /bin/echo -n .
-$cli set repeat.wait 18
+$cli set repeat.wait 30
 /bin/echo -n .
 $cli set remap.simple_vi_mode 1
 /bin/echo -n .
-$cli set remap.vim_emu_off_with_left_click 1
+$cli set option.tabmode_w 1
 /bin/echo -n .
 $cli set option.tabmode_uo 1
 /bin/echo -n .
-$cli set remap.vim_emu_jk_toggle 1
+$cli set remap.simultaneouskeypresses_fj_to_return 1
+/bin/echo -n .
+$cli set passthrough.holding_escape 1
 /bin/echo -n .
 /bin/echo
